93d03a67c3ffec46600772f8748aacfb3909ca77
dejan966 <59951914+dejan966@users.noreply.github.com>
2023-05-06 20:54:04 +0200

    Adding leaderboard on guesses

--- src/api/Guess.ts
+++ src/api/Guess.ts
@@ -3,11 +3,21 @@ import { apiRequest } from './Api'
 import { apiRoutes } from 'constants/apiConstants'
 import { GuessUserFields } from 'hooks/react-hook-form/useGuess'
 
-export const fetchPersonalBest = async (pageNumber:number) =>
-  apiRequest<never, GuessType[]>('get', `${apiRoutes.GUESSES_PREFIX}?page=${pageNumber}`)
+export const fetchPersonalBest = async (pageNumber: number) =>
+  apiRequest<never, GuessType[]>(
+    'get',
+    `${apiRoutes.ME_GUESSES}?page=${pageNumber}`,
+  )
 
-export const makeGuess = async (data:GuessUserFields, locationId:number) =>
-  apiRequest<GuessUserFields, void>('post', `${apiRoutes.GUESS_LOCATION_PREFIX}/${locationId}`, data)
+export const makeGuess = async (data: GuessUserFields, locationId: number) =>
+  apiRequest<GuessUserFields, void>(
+    'post',
+    `${apiRoutes.GUESS_LOCATION_PREFIX}/${locationId}`,
+    data,
+  )
 
-export const getLocationFromGuess = async (locationId:number) =>
-  apiRequest<never, GuessType>('get', `${apiRoutes.GUESS_LOCATION_PREFIX}/${locationId}`)
+export const getLocationFromGuess = async (locationId: number) =>
+  apiRequest<never, GuessType>(
+    'get',
+    `${apiRoutes.GUESS_LOCATION_PREFIX}/${locationId}`,
+  )
--- src/components/guess/GuessForm.tsx
+++ src/components/guess/GuessForm.tsx
@@ -118,207 +118,217 @@ const GuessForm: FC<Props> = ({ defaultValues }) => {
   })
 
   return (
-    <>
-      <h2 className="text-start">
-        Take a <span className="green">guess</span>!
-      </h2>
-      <Form onSubmit={onSubmit}>
-        {defaultValues ? (
-          <>
-            <Controller
-              control={control}
-              name="image_url"
-              render={({ field }) => (
-                <Form.Group className="mb-3">
-                  <input
-                    {...field}
-                    type="image"
-                    src={`${process.env.REACT_APP_API_URL}/uploads/locations/${defaultValues.location.image_url}`}
-                    width="100%"
-                    height="500"
-                    aria-label="Image_url"
-                    aria-describedby="image_url"
-                    className="mx-auto d-block"
-                  />
-                  {errors.image_url && (
-                    <div className="invalid-feedback text-danger">
-                      {errors.image_url.message}
-                    </div>
-                  )}
-                </Form.Group>
-              )}
-            />
-            <div className="mb-3">
-              {isLoaded && (
-                <GoogleMap
-                  mapContainerStyle={mapStyles}
-                  zoom={13}
-                  center={currentPosition}
-                  onClick={(e) => compareDistance(e)}
-                >
-                  <MarkerF position={currentPosition} />
-                </GoogleMap>
-              )}
-            </div>
-            <div className="d-flex justify-content-between">
-              <div className="col-md-3">
-                <Controller
-                  control={control}
-                  name="errorDistance"
-                  render={({ field }) => (
-                    <Form.Group className="mb-3">
-                      <FormLabel htmlFor="errorDistance">
-                        Error distance
-                      </FormLabel>
-                      <input
-                        {...field}
-                        type="text"
-                        value={distanceInMeters.distance}
-                        aria-label="Error Distance"
-                        aria-describedby="errorDistance"
-                        className={
-                          errors.errorDistance
-                            ? 'form-control is-invalid'
-                            : 'form-control'
-                        }
-                        readOnly
-                      />
-                      {errors.errorDistance && (
-                        <div className="invalid-feedback text-danger">
-                          {errors.errorDistance.message}
-                        </div>
-                      )}
-                    </Form.Group>
-                  )}
-                />
-              </div>
-              <div className="col-md-7">
-                <Form.Group className="mb-3">
-                  <FormLabel htmlFor="guessedLocation">
-                    Guessed location
-                  </FormLabel>
-                  <input
-                    value={address && address.location}
-                    name="Guessed location"
-                    type="text"
-                    aria-label="guessed_location"
-                    aria-describedby="guessed_location"
-                    className="form-control"
-                    readOnly
-                  />
-                </Form.Group>
-              </div>
-            </div>
-          </>
-        ) : (
-          <>
-            {locationData && (
-              <>
-                <Controller
-                  control={control}
-                  name="image_url"
-                  render={({ field }) => (
-                    <Form.Group className="mb-3">
-                      <input
-                        {...field}
-                        type="image"
-                        src={`${process.env.REACT_APP_API_URL}/uploads/locations/${locationData.data.image_url}`}
-                        width="100%"
-                        height="500"
-                        aria-label="Image_url"
-                        aria-describedby="image_url"
-                        className="mx-auto d-block"
-                      />
-                      {errors.image_url && (
-                        <div className="invalid-feedback text-danger">
-                          {errors.image_url.message}
-                        </div>
-                      )}
-                    </Form.Group>
-                  )}
-                />
-                <div className="mb-3">
-                  {isLoaded && (
-                    <GoogleMap
-                      mapContainerStyle={mapStyles}
-                      zoom={13}
-                      center={currentPosition}
-                      onClick={(e) => compareDistance(e)}
-                    >
-                      <MarkerF position={currentPosition} />
-                    </GoogleMap>
-                  )}
-                </div>
-                <div className="d-flex justify-content-between">
-                  <div className="col-md-3">
-                    <Controller
-                      control={control}
-                      name="errorDistance"
-                      render={() => (
-                        <Form.Group className="mb-3">
-                          <FormLabel htmlFor="errorDistance">
-                            Error distance
-                          </FormLabel>
-                          <input
-                            value={
-                              distanceInMeters && distanceInMeters.distance
-                            }
-                            type="text"
-                            aria-label="Error Distance"
-                            aria-describedby="errorDistance"
-                            className={
-                              errors.errorDistance
-                                ? 'form-control is-invalid'
-                                : 'form-control'
-                            }
-                            readOnly
-                          />
-                          {errors.errorDistance && (
-                            <div className="invalid-feedback text-danger">
-                              {errors.errorDistance.message}
-                            </div>
-                          )}
-                        </Form.Group>
-                      )}
+    <div className="d-flex justify-content-start">
+      <div className='col-md-9 me-3'>
+        <h2 className="text-start">
+          Take a <span className="green">guess</span>!
+        </h2>
+        <Form onSubmit={onSubmit}>
+          {defaultValues ? (
+            <>
+              <Controller
+                control={control}
+                name="image_url"
+                render={({ field }) => (
+                  <Form.Group className="mb-3">
+                    <input
+                      {...field}
+                      type="image"
+                      src={`${process.env.REACT_APP_API_URL}/uploads/locations/${defaultValues.location.image_url}`}
+                      width="100%"
+                      height="500"
+                      aria-label="Image_url"
+                      aria-describedby="image_url"
+                      className="mx-auto d-block"
                     />
-                  </div>
-                  <div className="col-md-7">
-                    <Form.Group className="mb-3">
-                      <FormLabel htmlFor="last_name">
-                        Guessed location
-                      </FormLabel>
-                      <input
-                        value={address && address.location}
-                        name="Guessed location"
-                        type="text"
-                        aria-label="guessed_location"
-                        aria-describedby="guessed_location"
-                        className="form-control"
-                      />
-                    </Form.Group>
-                  </div>
+                    {errors.image_url && (
+                      <div className="invalid-feedback text-danger">
+                        {errors.image_url.message}
+                      </div>
+                    )}
+                  </Form.Group>
+                )}
+              />
+              <div className="mb-3">
+                {isLoaded && (
+                  <GoogleMap
+                    mapContainerStyle={mapStyles}
+                    zoom={13}
+                    center={currentPosition}
+                    onClick={(e) => compareDistance(e)}
+                  >
+                    <MarkerF position={currentPosition} />
+                  </GoogleMap>
+                )}
+              </div>
+              <div className="d-flex justify-content-between">
+                <div className="col-md-3">
+                  <Controller
+                    control={control}
+                    name="errorDistance"
+                    render={({ field }) => (
+                      <Form.Group className="mb-3">
+                        <FormLabel htmlFor="errorDistance">
+                          Error distance
+                        </FormLabel>
+                        <input
+                          {...field}
+                          type="text"
+                          value={distanceInMeters.distance}
+                          aria-label="Error Distance"
+                          aria-describedby="errorDistance"
+                          className={
+                            errors.errorDistance
+                              ? 'form-control is-invalid'
+                              : 'form-control'
+                          }
+                          readOnly
+                        />
+                        {errors.errorDistance && (
+                          <div className="invalid-feedback text-danger">
+                            {errors.errorDistance.message}
+                          </div>
+                        )}
+                      </Form.Group>
+                    )}
+                  />
                 </div>
-              </>
-            )}
-          </>
+                <div className="col-md-7">
+                  <Form.Group className="mb-3">
+                    <FormLabel htmlFor="guessedLocation">
+                      Guessed location
+                    </FormLabel>
+                    <input
+                      value={address && address.location}
+                      name="Guessed location"
+                      type="text"
+                      aria-label="guessed_location"
+                      aria-describedby="guessed_location"
+                      className="form-control"
+                      readOnly
+                    />
+                  </Form.Group>
+                </div>
+              </div>
+            </>
+          ) : (
+            <>
+              {locationData && (
+                <>
+                  <Controller
+                    control={control}
+                    name="image_url"
+                    render={({ field }) => (
+                      <Form.Group className="mb-3">
+                        <input
+                          {...field}
+                          type="image"
+                          src={`${process.env.REACT_APP_API_URL}/uploads/locations/${locationData.data.image_url}`}
+                          width="100%"
+                          height="500"
+                          aria-label="Image_url"
+                          aria-describedby="image_url"
+                          className="mx-auto d-block"
+                        />
+                        {errors.image_url && (
+                          <div className="invalid-feedback text-danger">
+                            {errors.image_url.message}
+                          </div>
+                        )}
+                      </Form.Group>
+                    )}
+                  />
+                  <div className="mb-3">
+                    {isLoaded && (
+                      <GoogleMap
+                        mapContainerStyle={mapStyles}
+                        zoom={13}
+                        center={currentPosition}
+                        onClick={(e) => compareDistance(e)}
+                      >
+                        <MarkerF position={currentPosition} />
+                      </GoogleMap>
+                    )}
+                  </div>
+                  <div className="d-flex justify-content-between">
+                    <div className="col-md-3">
+                      <Controller
+                        control={control}
+                        name="errorDistance"
+                        render={() => (
+                          <Form.Group className="mb-3">
+                            <FormLabel htmlFor="errorDistance">
+                              Error distance
+                            </FormLabel>
+                            <input
+                              value={
+                                distanceInMeters && distanceInMeters.distance
+                              }
+                              type="text"
+                              aria-label="Error Distance"
+                              aria-describedby="errorDistance"
+                              className={
+                                errors.errorDistance
+                                  ? 'form-control is-invalid'
+                                  : 'form-control'
+                              }
+                              readOnly
+                            />
+                            {errors.errorDistance && (
+                              <div className="invalid-feedback text-danger">
+                                {errors.errorDistance.message}
+                              </div>
+                            )}
+                          </Form.Group>
+                        )}
+                      />
+                    </div>
+                    <div className="col-md-7">
+                      <Form.Group className="mb-3">
+                        <FormLabel htmlFor="last_name">
+                          Guessed location
+                        </FormLabel>
+                        <input
+                          value={address && address.location}
+                          name="Guessed location"
+                          type="text"
+                          aria-label="guessed_location"
+                          aria-describedby="guessed_location"
+                          className="form-control"
+                        />
+                      </Form.Group>
+                    </div>
+                  </div>
+                </>
+              )}
+            </>
+          )}
+          <div className="d-flex justify-content-end">
+            <Button className="btnRegister" type="submit">
+              Guess
+            </Button>
+          </div>
+        </Form>
+        {showError && (
+          <ToastContainer className="p-3" position="top-end">
+            <Toast onClose={() => setShowError(false)} show={showError}>
+              <Toast.Header>
+                <strong className="me-suto text-danger">Error</strong>
+              </Toast.Header>
+              <Toast.Body className="text-danger bg-light">
+                {apiError}
+              </Toast.Body>
+            </Toast>
+          </ToastContainer>
         )}
-        <div className="d-flex justify-content-end">
-          <Button className="btnRegister" type="submit">
-            Guess
-          </Button>
+      </div>
+      <div>
+        <h2 className="text-start">Leaderboard</h2>
+        <div>
+          {/* leaderboard */}
         </div>
-      </Form>
-      {showError && (
-        <ToastContainer className="p-3" position="top-end">
-          <Toast onClose={() => setShowError(false)} show={showError}>
-            <Toast.Header>
-              <strong className="me-suto text-danger">Error</strong>
-            </Toast.Header>
-            <Toast.Body className="text-danger bg-light">{apiError}</Toast.Body>
-          </Toast>
-        </ToastContainer>
-      )}
-    </>
+      </div>
+    </div>
   )
 }
 
